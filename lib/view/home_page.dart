577be04e384e8bbf632e26d694cba21c78ad03b4
Thomas Erhel <thomas.erhel@gmail.com>
2021-09-26 14:54:53 +0200

    First commit ✨

---
+++ lib/view/home_page.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'dart:math';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key, required this.themeChanged}) : super(key: key);
+
+  final void Function(String themeName) themeChanged;
+
+  @override
+  // ignore: no_logic_in_create_state
+  _HomePageState createState() => _HomePageState(themeChanged);
+}
+
+class _HomePageState extends State<HomePage> {
+  _HomePageState(this.themeChanged);
+
+  var themeName = 'Yaru-light';
+  final void Function(String themeName) themeChanged;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Dicee'),
+        centerTitle: true,
+        leading: TextButton(
+          child: Icon(themeName.contains('-light')
+              ? Icons.dark_mode
+              : Icons.light_mode),
+          onPressed: () => {
+            setState(() {
+              if (themeName.contains('-light')) {
+                themeName = 'Yaru-dark';
+              } else if (themeName.contains('-dark')) {
+                themeName = 'Yaru-light';
+              }
+              themeChanged(themeName);
+            })
+          },
+        ),
+      ),
+      body: const DicePage(),
+    );
+  }
+}
+
+class DicePage extends StatefulWidget {
+  const DicePage({Key? key}) : super(key: key);
+
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void changeDiceFace() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              child: Image.asset(
+                'images/dice$leftDiceNumber.png',
+              ),
+              onPressed: () {
+                changeDiceFace();
+              },
+            ),
+          ),
+          //Get students to create the second die as a challenge
+          Expanded(
+            child: FlatButton(
+              child: Image.asset(
+                'images/dice$rightDiceNumber.png',
+              ),
+              onPressed: () {
+                changeDiceFace();
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
